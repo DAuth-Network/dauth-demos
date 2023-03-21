f5a33fb36a77a80d3b351c7f4dcf7d302dc34e34
scott9527 <scott@dauth.network>
2023-03-22 00:33:54 +0800

    nm

--- src/components/SignatureData/index.tsx
+++ src/components/SignatureData/index.tsx
@@ -54,7 +54,7 @@ const SignatureData: FC<ISignatureData> = ({ data }) => {
             </div>
             <div className='lg:p-20 lg:h-full  bg-[#1f1f1f]  rounded-lg p-4'>
                 {
-                   dataToShow ?  <JsonItem item={dataToShow} /> : <span className={`${firaCode.className} text-sm`}>// Verify your accounts to see DAuth in actions</span>
+                   dataToShow ?  <JsonItem item={dataToShow} /> : <span className={`${firaCode.className} text-sm`}>{'// Verify your accounts to see DAuth in actions'}</span>
                 }
             </div>
 
