88c98105fb16d7e926d8722a515bc8f69e95ea1b
scott9527 <scott@dauth.network>
2023-03-23 01:45:39 +0800

    Fix redirect url error

--- src/services/http/oauth.ts
+++ src/services/http/oauth.ts
@@ -1,4 +1,5 @@
 export const githubLogin = async () => {
+    const host = window.location.protocol + '//' +  window.location.host + '/oauth/github'
     const CLIENT_ID = 'aeb37125b47b33e6c3fd'
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${'http://localhost:3000/oauth/github'}&scope=user`;
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${host}&scope=user`;
 }
