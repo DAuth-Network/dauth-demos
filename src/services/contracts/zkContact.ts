0f1e3f85adbde5f87e10ddf16f7004212d66a8d8
Dean <yyd106@126.com>
2023-12-01 10:15:55 +0800

    change address

--- src/services/contracts/zkContact.ts
+++ src/services/contracts/zkContact.ts
@@ -1,5 +1,5 @@
 
 import abi from "./zkadmin.abi.json";
 
-export const testContractAddress = '0xbd03AEBC59778f3Aa96E65D29F3CFEcA093a4372'
+export const testContractAddress = '0x035F33fbC1b96A3356169f498D4689dcb7079927'
 export const ContractAddress = '0x7295488ba29c0903a361a2d5ace9fe3e9667ae7a'
