b1af9e08e69ebad241cdd78cc2804cd923ad1c22
yyd106 <yyd106@gmail.com>
2024-03-25 16:36:38 +0800

    Update discord link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import {IoLogoDiscord} from "react-icons/io5";
 const Footer = () => {
     return (
         <footer className={'flex justify-center flex-shrink-0 my-10'}>
-            <a href="https://discord.gg/WXzQCzeJ" target={"_blank"} className={'px-4'}>
+            <a href="https://discord.gg/AJv2tUE4cm" target={"_blank"} className={'px-4'}>
                 <IoLogoDiscord size={28}/>
             </a>
 
